8909e580942b916a50dc60034bb7041f4b8e55d8
jrc <jc101@utas.edu.au>
2025-03-24 17:35:19 +1100

    Add js_intro required files

---
+++ js_intro/js/playground.js
@@ -0,0 +1,50 @@
+/* Demonstration playground for Tutorial 4 */
+
+console.log("playground.js is running");
+
+
+/* Activity 2: Football Trophy */
+
+// Activity 2, Rule 1
+
+function average(values) {
+    //TODO Implement this function
+}
+
+function determineRule1Winner() {
+    //TODO Write your code for determining the winner of the two teams and updating the page here
+}
+
+// Activity 2, Rule 2
+
+function wins(scores1, scores2) {
+    //TODO Implement this function
+}
+
+function determineRule2Winner() {
+    //TODO Write your code for determining the winner of the two teams under the new conditions and updating the page here
+}
+
+//TODO Call the top-level functions determineRule1Winner and determinRule2Winner when they're ready
+
+
+
+
+/* Activity 3: Tax */
+
+
+let clients = { //TODO Record client incomes here
+
+};
+
+//TODO Write your other functions for Activity 3 here
+
+
+
+/* Activity 4: Arbitrary HTML */
+
+//TODO Write your function for Activity 4 here
+
+
+
+// And call that function here
